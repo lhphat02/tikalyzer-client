aa7f2f58b435a2e1a46f3a22a9a5914307fd04ab
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-06-07 20:49:24 +0700

    refactor: Change slide layout

--- src/components/landing/CategorySlide.jsx
+++ src/components/landing/CategorySlide.jsx
@@ -6,39 +6,49 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/navigation';
+import { useInView } from 'react-intersection-observer';
 
 const featureCategories = [
   {
     name: 'User Analytics',
     path: '/analyze-user',
-    icon: <UserCircleIcon class="h-6 w-6" />,
+    icon: <UserCircleIcon className="h-6 w-6" />,
   },
   {
     name: 'HashTag Analytics',
     path: '/analyze-hashtag',
-    icon: <HashtagIcon class="h-6 w-6" />,
+    icon: <HashtagIcon className="h-6 w-6" />,
   },
   {
-    name: 'Trending Analytics',
+    name: 'Trend Analytics',
     path: '/analyze-trending',
-    icon: <FireIcon class="h-6 w-6" />,
+    icon: <FireIcon className="h-6 w-6" />,
   },
 ];
 
 const CategorySlide = ({ onSlideChange }) => {
   const router = useRouter();
+  const { ref, inView } = useInView({
+    threshold: 0.1, // Adjust the threshold as needed
+  });
 
   return (
     <div className="items-center justify-center h-full gap-8 page-container">
-      <h2 className="text-lg text-prim-1 md:text-3xl">
-        Explore our analytic categories
+      <h2
+        className={`text-lg text-prim-1 md:text-3xl transition-all duration-1000 ease-in-out ${
+          inView ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        Explore our analytic tools
       </h2>
 
-      <div className="flex flex-wrap justify-center gap-4">
+      <div className="flex flex-wrap justify-center gap-4 " ref={ref}>
         {featureCategories.map((category, index) => (
           <Button
             key={index}
-            className="rounded-md md:py-4 hover:shadow-lg"
+            className={`rounded-md md:py-4 hover:shadow-lg transform transition-all duration-500 ease-in-out ${
+              inView ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
+            }`}
             onClick={() => router.push(category.path)}
           >
             {category.icon}
@@ -46,9 +56,6 @@ const CategorySlide = ({ onSlideChange }) => {
           </Button>
         ))}
       </div>
-      {/* <Button outline className="rounded-md md:py-4 hover:shadow-lg">
-        <p className="text-xs md:text-base">See all categories</p>
-      </Button> */}
 
       <div
         className="flex justify-center gap-4 text-xl font-semibold underline cursor-pointer underline-offset-2 text-prim-1 hover:text-prim-2"
